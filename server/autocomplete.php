505e222865795b46e467c1b8357803751b451a2b
Oliver Albrecht <oliver.albrecht.berlin@gmail.com>
2011-05-18 12:37:28 +0200

    Added missing files

---
+++ server/autocomplete.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Script for the autocompletion in jQuery Plugin tagedit.
+ *
+ * @author Oliver Albrecht <info@webwork-albrecht.de>
+ * @version 1.0.0
+ * @copyright Copyright (c) 2010, Oliver Albrecht
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+$autocompletiondata = array(
+    3 => 'Hazel Grouse',
+    4 => 'Common Quail',
+    5 => 'Common Pheasant',
+    6 => 'Northern Shoveler',
+    7 => 'Greylag Goose',
+    8 => 'Barnacle Goose',
+    9 => 'Lesser Spotted Woodpecker',
+    10 => 'Eurasian Pygmy-Owl',
+    11 => 'Dunlin',
+    13 => 'Black Scoter',
+    14 => 'Eurasian Wryneck',
+    15 => 'Little Owl',
+    16 => 'Eurasian Curlew',
+    17 => 'Ruff',
+    18 => 'Little Tern',
+    19 => 'Merlin',
+    20 => 'Bluethroat',
+    21 => 'Redwing',
+    22 => 'Wood Nuthatch',
+    23 => 'Firecrest',
+    24 => 'Goldcrest',
+    25 => 'Bearded Parrotbill',
+    26 => 'Chaffinch',
+    27 => 'Brambling',
+    28 => 'Hawfinch',
+    29 => 'Goldcrest',
+);
+
+if(isset($_GET['term'])) {
+    $result = array();
+    foreach($autocompletiondata as $key => $value) {
+        if(strlen($_GET['term']) == 0 || strpos(strtolower($value), strtolower($_GET['term'])) !== false) {
+            $result[] = '{"id":"'.$key.'","label":"'.$value.'","value":"'.$value.'"}';
+        }
+    }
+    
+    echo "[".implode(',', $result)."]";
+}
+?>
---
+++ server/formtarget.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Script for evaluationg the form that is send from jQuery Plugin tagedit.
+ *
+ * @author Oliver Albrecht <info@webwork-albrecht.de>
+ * @version 1.0.0
+ * @copyright Copyright (c) 2010, Oliver Albrecht
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+//echo '<pre>' . print_r($_POST['tag'], true) . '</pre>';exit;
+
+if(array_key_exists('save', $_POST) && array_key_exists('tag', $_POST)) {
+    // Include the autocompleteScript to know what was in the database
+    include ('autocomplete.php');
+    
+    $result = array('new' => array(), 'deleted' => array(), 'changed' => array(), 'not changed' => array());
+    foreach($_POST['tag'] as $key => $value) {
+        if(preg_match('/([0-9]*)-?(a|d)?$/', $key, $keyparts) === 1) {
+            if(isset($keyparts[2])) {
+                switch($keyparts[2]) {
+                    case 'a':
+                        if($autocompletiondata[$keyparts[1]] != $value) {
+                                // Items has changed
+                            $result['changed'][] = $keyparts[1] . ' (new value: "' . $value . '")';
+                        }
+                        else {
+                            $result['not changed'][] = $keyparts[1] . ' ("' . $value . '")';
+                        }
+                        break;
+                    case 'd':
+                        $result['deleted'][] = $keyparts[1] . ' ("' . $value . '")';
+                        break;
+                }
+            }
+            else {
+                $result['new'][] = $key . ' ("' . $value . '")';
+            }
+        }
+    }
+}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
+<head>
+    <title>jQuery Plugin tagedit - Tageditor</title>
+    
+    <meta name="generator" content="Komodo IDE"  />
+    <meta name="author" content="Oliver Albrecht" />
+    <meta name="description" content="Example Page for the jQuery Plugin tagedit. It offers an inputfield for adding, editing und deleting keywords, tags and other lists. There is also autocompletion" />
+    <meta name="robots" content="index, follow" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+    <meta http-equiv="content-language" content="de"/>
+    <meta name="language" content="de"/>
+    
+    <link rel="shortcut icon" href="../favicon.ico"/>
+    
+    <link rel="StyleSheet" href="../css/jquery.tagedit-1.0.0.css" type="text/css" media="all"/>
+    </head>
+<body>
+<h1>Tagedit result</h1>
+<a href="../index.html" title="Back">Back to inputpage</a>
+<?php if(array_key_exists('save', $_POST) && array_key_exists('tag', $_POST) && count($_POST['tag']) > 0) :?>
+    <p>The following inputs where received:</p>
+    <?php foreach($result as $key => $results) : ?>
+        <h2>Tags that are <?php echo $key; ?></h2>
+        <?php if(count($results) > 0) :?>
+            <ul>
+                <?php foreach($results as $tag): ?>
+                    <li>Id: <?php echo $tag; ?></li>
+                <?php endforeach; ?>
+            </ul>
+        <?php else: ?>
+            <p>nothing</p>
+        <?php endif; //count(dleted) > 0?>
+    <?php endforeach; ?>
+    
+    
+<?php else: ?>
+    <p>You did not enter any data</p>
+<?php endif; // $_POST['tag'] ?>
+
+</body>
+</html>
