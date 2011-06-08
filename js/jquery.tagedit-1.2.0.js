115d9af155d77225c5746f8008e849c58847efa4
Steven Black <steveb@stevenblack.com>
2011-06-08 12:11:35 -0400

    Incorrect application of options.animSpeed onto the blur event setTimeout.
    Signed-off-by: Steven Black <steveb@stevenblack.com>

--- js/jquery.tagedit-1.2.0.js
+++ js/jquery.tagedit-1.2.0.js
@@ -243,7 +243,7 @@
 							else {
 								// Delete entry after a timeout
 								var input = $(this);
-								$(this).data('blurtimer', window.setTimeout(function() {input.val('');}, options.animSpeed));
+								$(this).data('blurtimer', window.setTimeout(function() {input.val('');}, 500));
 							}
 						})
 						.focus(function() {
@@ -363,7 +363,7 @@
 					})
 					.blur(function() {
 						var that = $(this);
-						closeTimer = window.setTimeout(function() {that.parent().trigger('finishEdit', [true])}, options.animSpeed);
+						closeTimer = window.setTimeout(function() {that.parent().trigger('finishEdit', [true])}, 500);
 					});
 		}
 
